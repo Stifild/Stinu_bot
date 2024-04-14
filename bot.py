27cafb8659bf133e78030585fd0f3249ae9e68e9
Stifild <fedorovkirillwww@yandex.ru>
2024-04-14 03:31:13 +0300

    PEP

--- bot.py
+++ bot.py
@@ -2,86 +2,186 @@ import telebot, logging, iop, json
 from config import LOGS_PATH
 from iop import IOP as io
 
-logging.basicConfig(filename=LOGS_PATH, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filemode="w")
+logging.basicConfig(
+    filename=LOGS_PATH,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filemode="w",
+)
 
 bot = telebot.TeleBot(io.bot_token)
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет! Я бот для работы с SpeachKit. Напиши /help для подробностей')
+    bot.send_message(
+        message.chat.id,
+        "Привет! Я бот для работы с SpeachKit. Напиши /help для подробностей",
+    )
 
-@bot.message_handler(commands=['help'])
+
+@bot.message_handler(commands=["help"])
 def help(message):
-    bot.send_message(message.chat.id, "Список команд:\n/tts <текст> - озвучить текст\n/menu - показать меню")
+    bot.send_message(
+        message.chat.id,
+        "Список команд:\n/tts <текст> - озвучить текст\n/menu - показать меню",
+    )
 
-@bot.message_handler(commands=['tts']) 
+
+@bot.message_handler(commands=["tts"])
 def tts(message):
     result: bool | tuple[bool, str] = io.tts(message)
     if result is not tuple:
-        bot.send_message(message.chat.id, 'Лови результат:')
-        bot.send_voice(message.chat.id, f"./data/temp/{message.from_user.id}.mp3", inline_keyboard=telebot.util.quick_markup({'Меню': {'callback_data': 'menu'}}))
+        bot.send_message(message.chat.id, "Лови результат:")
+        bot.send_voice(
+            message.chat.id,
+            f"./data/temp/{message.from_user.id}.mp3",
+            inline_keyboard=telebot.util.quick_markup(
+                {"Меню": {"callback_data": "menu"}}
+            ),
+        )
     else:
-        bot.send_message(message.chat.id, result[1], 
-                         inline_keyboard=telebot.util.quick_markup({'Вики по кодам ошибок': {'url': 'https://ru.wikipedia.org/wiki/Список_кодов_состояния_HTTP#Обзорный_список'}})\
-                           if "кодом:" in result[1] else None)
-    
-@bot.callback_query_handler(func=lambda call: call.data == 'menu')
-@bot.message_handler(commands=['menu'])
-def menu(call):
-    message: telebot.types.Message = call.message if call.message else call.callback_query.message
-    bot.send_message(message.chat.id, 'Меню:', inline_keyboard=io.get_inline_keyboard((("Выбрать голос", 'voice'), ("Выбрать эмоцию", 'emotion'), ("Выбрать скорость", 'speed'))))
+        bot.send_message(
+            message.chat.id,
+            result[1],
+            inline_keyboard=(
+                telebot.util.quick_markup(
+                    {
+                        "Вики по кодам ошибок": {
+                            "url": "https://ru.wikipedia.org/wiki/Список_кодов_состояния_HTTP#Обзорный_список"
+                        }
+                    }
+                )
+                if "кодом:" in result[1]
+                else None
+            ),
+        )
 
-@bot.callback_query_handler(func=lambda call: call.data == 'voice')
+
+@bot.callback_query_handler(func=lambda call: call.data == "menu")
+@bot.message_handler(commands=["menu"])
+def menu(call):
+    message: telebot.types.Message = (
+        call.message if call.message else call.callback_query.message
+    )
+    bot.send_message(
+        message.chat.id,
+        "Меню:",
+        inline_keyboard=io.get_inline_keyboard(
+            (
+                ("Выбрать голос", "voice"),
+                ("Выбрать эмоцию", "emotion"),
+                ("Выбрать скорость", "speed"),
+            )
+        ),
+    )
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "voice")
 def choose_voice(call):
-    message: telebot.types.Message = call.message if call.message else call.callback_query.message
+    message: telebot.types.Message = (
+        call.message if call.message else call.callback_query.message
+    )
     bot.delete_message(message.chat.id, message.message_id)
-    bot.send_message(message.chat.id, 'Выбери голос:', reply_markup=io.get_reply_markup(io.tuple_voices()))
+    bot.send_message(
+        message.chat.id,
+        "Выбери голос:",
+        reply_markup=io.get_reply_markup(io.tuple_voices()),
+    )
     bot.register_next_step_handler(message, select_voice)
+
 
 def select_voice(message):
     if message.text in io.tuple_voices():
         io.db[str(message.from_user.id)]["voice"] = message.text
         bot.send_message(message.chat.id, f'Теперь используется голос "{message.text}"')
-        bot.send_message(message.chat.id, 'Меню:', inline_keyboard=io.get_inline_keyboard((("Выбрать голос", 'voice'), ("Выбрать эмоцию", 'emotion'), ("Выбрать скорость", 'speed'))))
+        bot.send_message(
+            message.chat.id,
+            "Меню:",
+            inline_keyboard=io.get_inline_keyboard(
+                (
+                    ("Выбрать голос", "voice"),
+                    ("Выбрать эмоцию", "emotion"),
+                    ("Выбрать скорость", "speed"),
+                )
+            ),
+        )
     else:
-        bot.send_message(message.chat.id, 'Неверный выбор. Попробуй ещё раз.', reply_markup=io.get_reply_markup(io.tuple_voices()))
+        bot.send_message(
+            message.chat.id,
+            "Неверный выбор. Попробуй ещё раз.",
+            reply_markup=io.get_reply_markup(io.tuple_voices()),
+        )
         bot.register_next_step_handler(message, select_voice)
 
-@bot.callback_query_handler(func=lambda call: call.data == 'emotion')
+
+@bot.callback_query_handler(func=lambda call: call.data == "emotion")
 def choose_emotion(call):
-    message: telebot.types.Message = call.message if call.message else call.callback_query.message
+    message: telebot.types.Message = (
+        call.message if call.message else call.callback_query.message
+    )
     bot.delete_message(message.chat.id, message.message_id)
-    bot.send_message(message.chat.id, 'Выбери эмоцию:', reply_markup=io.get_reply_markup(io.tuple_emotions(message.from_user.id)))
+    bot.send_message(
+        message.chat.id,
+        "Выбери эмоцию:",
+        reply_markup=io.get_reply_markup(io.tuple_emotions(message.from_user.id)),
+    )
     bot.register_next_step_handler(message, select_emotion)
+
 
 def select_emotion(message):
     if message.text in io.tuple_emotions(message.from_user.id):
         io.db[str(message.from_user.id)]["emotion"] = message.text
-        bot.send_message(message.chat.id, f'Теперь используется эмоция "{message.text}"')
-        bot.send_message(message.chat.id, 'Меню:', inline_keyboard=io.get_inline_keyboard((("Выбрать голос", 'voice'), ("Выбрать эмоцию", 'emotion'), ("Выбрать скорость", 'speed'))))
+        bot.send_message(
+            message.chat.id, f'Теперь используется эмоция "{message.text}"'
+        )
+        bot.send_message(
+            message.chat.id,
+            "Меню:",
+            inline_keyboard=io.get_inline_keyboard(
+                (
+                    ("Выбрать голос", "voice"),
+                    ("Выбрать эмоцию", "emotion"),
+                    ("Выбрать скорость", "speed"),
+                )
+            ),
+        )
     else:
-        bot.send_message(message.chat.id, 'Неверный выбор. Попробуй ещё раз.', reply_markup=io.get_reply_markup(io.tuple_emotions(message.from_user.id)))
+        bot.send_message(
+            message.chat.id,
+            "Неверный выбор. Попробуй ещё раз.",
+            reply_markup=io.get_reply_markup(io.tuple_emotions(message.from_user.id)),
+        )
         bot.register_next_step_handler(message, select_emotion)
 
-@bot.callback_query_handler(func=lambda call: call.data =='speed')
+
+@bot.callback_query_handler(func=lambda call: call.data == "speed")
 def choose_speed(call):
-    message: telebot.types.Message = call.message if call.message else call.callback_query.message
+    message: telebot.types.Message = (
+        call.message if call.message else call.callback_query.message
+    )
     bot.delete_message(message.chat.id, message.message_id)
-    bot.send_message(message.chat.id, 'Задай скорость от 0.1 до 3')
+    bot.send_message(message.chat.id, "Задай скорость от 0.1 до 3")
     bot.register_next_step_handler(message, select_speed)
+
 
 def select_speed(message):
     if float(message.text) >= 0.1 and float(message.text) <= 3.0:
         io.db[str(message.from_user.id)]["speed"] = message.text
-        bot.send_message(message.chat.id, f'Теперь используется скорость "{message.text}"')
-        bot.send_message(message.chat.id, 'Меню:', inline_keyboard=io.get_inline_keyboard((("Выбрать голос", 'voice'), ("Выбрать эмоцию", 'emotion'), ("Выбрать скорость", 'speed'))))
+        bot.send_message(
+            message.chat.id, f'Теперь используется скорость "{message.text}"'
+        )
+        bot.send_message(
+            message.chat.id,
+            "Меню:",
+            inline_keyboard=io.get_inline_keyboard(
+                (
+                    ("Выбрать голос", "voice"),
+                    ("Выбрать эмоцию", "emotion"),
+                    ("Выбрать скорость", "speed"),
+                )
+            ),
+        )
     else:
-        bot.send_message(message.chat.id, 'Неверный выбор. Попробуй ещё раз.')
+        bot.send_message(message.chat.id, "Неверный выбор. Попробуй ещё раз.")
         bot.register_next_step_handler(message, select_speed)
- 
-
-                
-
-
-
-        
--- iop.py
+++ iop.py
@@ -1,40 +1,56 @@
 import logging, json, requests, os, telebot, time
-from config import LOGS_PATH, JSON_PATH, FOLDER_ID, TELEGRAM_TOKEN, IAM_TOKEN_PATH, VJOSN_PATH
+from config import (
+    LOGS_PATH,
+    JSON_PATH,
+    FOLDER_ID,
+    TELEGRAM_TOKEN,
+    IAM_TOKEN_PATH,
+    VJOSN_PATH,
+)
 
 os.mkdir("./data/temp")
 
-logging.basicConfig(filename=LOGS_PATH, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filemode="w")
+logging.basicConfig(
+    filename=LOGS_PATH,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filemode="w",
+)
+
 
 class IOP:
 
     def __init__(self):
         self.db = self.read_json()
-        self.bot_token = TELEGRAM_TOKEN            
+        self.bot_token = TELEGRAM_TOKEN
 
     def write_json(self, data: dict, path: str = JSON_PATH):
-        with open(path, 'w') as f:
+        with open(path, "w") as f:
             json.dump(data, f, indent=4)
-        
+
     def read_json(self, path: str = JSON_PATH):
         try:
-            with open(path, 'r') as f:
+            with open(path, "r") as f:
                 return json.load(f)
         except json.decoder.JSONDecodeError:
             return {}
-    
+
     def sing_up(self, id: int):
         self.db[str(id)] = {
             "limit": 500,
             "ban": False,
             "voice": None,
-
         }
         self.write_json(self.db)
 
     def tts(self, message: telebot.types.Message) -> bool | tuple[bool, str]:
         text = telebot.util.extract_arguments(message.text)
         id = message.from_user.id
-        if len(text) > 0 and len(text) < 251 and len(text) < int(self.db[str(id)]["limit"]):
+        if (
+            len(text) > 0
+            and len(text) < 251
+            and len(text) < int(self.db[str(id)]["limit"])
+        ):
             status, result = SpeechKit.text_to_speech(text, str(id))
             if status:
                 with open(f"./data/temp/{str(id)}.ogg", "wb") as f:
@@ -46,7 +62,7 @@ class IOP:
                 return False, result
         else:
             return False, "Текст должен быть от 1 до 250 символов"
-        
+
     def get_iam_token(self) -> str:
         try:
             with open(IAM_TOKEN_PATH, "r") as token_file:
@@ -64,14 +80,22 @@ class IOP:
             token_data = json.load(token_file)
 
         return token_data.get("access_token")
-    
-    def get_inline_keyboard(self, values: tuple[tuple[str, str]]) -> telebot.types.InlineKeyboardMarkup:
+
+    def get_inline_keyboard(
+        self, values: tuple[tuple[str, str]]
+    ) -> telebot.types.InlineKeyboardMarkup:
         markup = telebot.types.InlineKeyboardMarkup()
         for value in values:
-            markup.add(telebot.types.InlineKeyboardButton(text=value[0], callback_data=value[1]))
+            markup.add(
+                telebot.types.InlineKeyboardButton(
+                    text=value[0], callback_data=value[1]
+                )
+            )
         return markup
-    
-    def get_reply_markup(self, values: tuple[str]) -> telebot.types.ReplyKeyboardMarkup | None:
+
+    def get_reply_markup(
+        self, values: tuple[str]
+    ) -> telebot.types.ReplyKeyboardMarkup | None:
         if values:
             markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
             for value in values:
@@ -79,18 +103,16 @@ class IOP:
             return markup
         else:
             return None
-                                
-    
+
     def tuple_voices(self) -> tuple[str]:
         return tuple(self.read_json(VJOSN_PATH).keys())
-    
+
     def tuple_emotions(self, id: int) -> tuple[str]:
         return tuple(self.read_json(VJOSN_PATH)[self.db[str(id)]["voice"]])
 
 
-
 class SpeechKit:
-    
+
     def text_to_speech(text: str, id: str):
         iam_token = IOP.get_iam_token()
         folder_id = FOLDER_ID
@@ -98,24 +120,28 @@ class SpeechKit:
         emotion = str(IOP.db[id]["emotion"])
         speed = str(IOP.db[id]["speed"])
 
-
         headers = {
-            'Authorization': f'Bearer {iam_token}',
+            "Authorization": f"Bearer {iam_token}",
         }
         data = {
-            'text': text,  # текст, который нужно преобразовать в голосовое сообщение
-            'lang': 'ru-RU',  # язык текста - русский
-            'voice': voice,
-            'emotion': emotion,
-            'speed': speed,
-            'folderId': folder_id,
+            "text": text,  # текст, который нужно преобразовать в голосовое сообщение
+            "lang": "ru-RU",  # язык текста - русский
+            "voice": voice,
+            "emotion": emotion,
+            "speed": speed,
+            "folderId": folder_id,
         }
-        response = requests.post('https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize', headers=headers, data=data)
+        response = requests.post(
+            "https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize",
+            headers=headers,
+            data=data,
+        )
 
         if response.status_code == 200:
             return True, response.content
         else:
             logging.error(response.content)
-            return False, f"При запросе в SpeechKit возникла ошибка c кодом: {response.status_code}"
-        
-    
+            return (
+                False,
+                f"При запросе в SpeechKit возникла ошибка c кодом: {response.status_code}",
+            )
