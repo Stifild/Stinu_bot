49ba7d13f27fc97e4a1d51f5015f671cdb90f2c7
Kirill <fedorovkirillwww@yandex.ru>
2024-05-14 22:32:37 +0300

    blob#47

--- bot.py
+++ bot.py
@@ -279,7 +279,7 @@ def gptp(message: telebot.types.Message):
         text: tuple[bool, str] = sk.stt(message, bot)
         if text[0]:
             bot.send_chat_action(message.chat.id, "typing")
-            answer = gpt.asking_gpt(message.from_user.id, text[1])
+            answer = gpt.asking_gpt(message.from_user.id, text[1], 1)
             bot.send_message(message.chat.id, answer)
             bot.send_chat_action(message.chat.id, "record_voice")
             result: bool | tuple[bool, str] = sk.tts(answer, 1, message.from_user.id)
